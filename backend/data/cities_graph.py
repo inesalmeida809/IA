0c4b05563eb7badd67cafe08a4724dff772072b7
INeS <pv25225@alunos.estgv.ipv.pt>
2026-03-13 15:31:57 +0000

    distancias

--- backend/data/cities_graph.py
+++ backend/data/cities_graph.py
@@ -1,1 +1,124 @@
-#create dicts
+cidades_vizinhas = {
+
+    "Aveiro": {
+        "Porto": 68,
+        "Viseu": 95,
+        "Coimbra": 68,
+        "Leiria": 115
+    },
+
+    "Braga": {
+        "Viana do Castelo": 48,
+        "Vila Real": 106,
+        "Porto": 53
+    },
+
+    "Bragança": {
+        "Vila Real": 137,
+        "Guarda": 202
+    },
+
+    "Beja": {
+        "Évora": 78,
+        "Faro": 152,
+        "Setúbal": 142
+    },
+
+    "Castelo Branco": {
+        "Coimbra": 159,
+        "Guarda": 106,
+        "Portalegre": 80,
+        "Évora": 203
+    },
+
+    "Coimbra": {
+        "Aveiro": 68,
+        "Viseu": 96,
+        "Leiria": 67,
+        "Castelo Branco": 159
+    },
+
+    "Évora": {
+        "Lisboa": 150,
+        "Santarém": 117,
+        "Portalegre": 131,
+        "Setúbal": 103,
+        "Beja": 78,
+        "Castelo Branco": 203
+    },
+
+    "Faro": {
+        "Beja": 152,
+        "Setúbal": 249,
+        "Lisboa": 299
+    },
+
+    "Guarda": {
+        "Vila Real": 157,
+        "Viseu": 85,
+        "Bragança": 202,
+        "Castelo Branco": 106
+    },
+
+    "Leiria": {
+        "Aveiro": 115,
+        "Coimbra": 67,
+        "Lisboa": 129,
+        "Santarém": 70
+    },
+
+    "Lisboa": {
+        "Santarém": 78,
+        "Setúbal": 50,
+        "Évora": 150,
+        "Faro": 299,
+        "Leiria": 129
+    },
+
+    "Porto": {
+        "Aveiro": 68,
+        "Braga": 53,
+        "Viana do Castelo": 71,
+        "Vila Real": 116,
+        "Viseu": 133
+    },
+
+    "Portalegre": {
+        "Castelo Branco": 80,
+        "Évora": 131
+    },
+
+    "Santarém": {
+        "Lisboa": 78,
+        "Leiria": 70,
+        "Évora": 117
+    },
+
+    "Setúbal": {
+        "Lisboa": 50,
+        "Évora": 103,
+        "Beja": 142,
+        "Faro": 249
+    },
+
+    "Viana do Castelo": {
+        "Braga": 48,
+        "Porto": 71
+    },
+
+    "Vila Real": {
+        "Braga": 106,
+        "Bragança": 137,
+        "Guarda": 157,
+        "Porto": 116,
+        "Viseu": 110
+    },
+
+    "Viseu": {
+        "Aveiro": 95,
+        "Coimbra": 96,
+        "Guarda": 85,
+        "Porto": 133,
+        "Vila Real": 110
+    }
+}
