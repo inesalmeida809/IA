1d402d92bc585c48ac22af769dd261b5a0928b95
Catarina Costa <catarinac.2005@gmail.com>
2026-03-27 15:48:48 +0000

    custo uniforme

---
+++ backend/algorithms/custo_uniforme.py
@@ -0,0 +1,30 @@
+from data.cities_graph import cidades_vizinhas
+
+def custo_uniforme(cidade_partida, cidade_chegada):
+    print(f"Eu quero ir de {cidade_partida} para {cidade_chegada}!")
+    dict_caminho = [(0, cidade_partida, [cidade_partida])]
+    visitados = {}
+
+    while dict_caminho:
+        dict_caminho.sort(key=lambda x: x[0])
+
+        custo_atual, cidade_atual, caminho = dict_caminho.pop(0)
+
+        if cidade_atual == cidade_chegada:
+            print(f"Chegou ao destino!\nCaminho: {' -> '.join(caminho)}\nCusto: {custo_atual} km")
+            return caminho, custo_atual
+        
+        if cidade_atual in visitados and visitados[cidade_atual] <= custo_atual:
+            continue
+
+        visitados[cidade_atual] = custo_atual
+
+        for vizinho, custo in cidades_vizinhas[cidade_atual].items():
+            novo_custo = custo_atual + custo
+            novo_caminho = caminho + [vizinho]
+            dict_caminho.append((novo_custo, vizinho, novo_caminho))
+
+    print("Não existe caminho.")
+    return None, None
+
+custo_uniforme("Aveiro", "Faro")
