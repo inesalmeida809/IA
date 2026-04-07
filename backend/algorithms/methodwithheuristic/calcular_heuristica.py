be88f28d71f349c4ca254ff59d1ec961fb7ef107
Diogo Oliveira <dfso2013@gmail.com>
2026-04-07 02:04:25 +0100

    o

--- backend/algorithms/methodwithheuristic/calcular_heuristica.py
+++ backend/algorithms/methodwithheuristic/calcular_heuristica.py
@@ -1,10 +1,26 @@
 #Aqui temos a função responsável por calcular a distância em linha reta de uma cidade à outra através das suas coordenadas
 
 import math
+
 from ...data.city_coordinates import coordenadas
 
 def calcular_heuristica(cidade_atual, cidade_destino):
     lat1, lon1 = coordenadas[cidade_atual]
     lat2, lon2 = coordenadas[cidade_destino]
 
-    return math.sqrt((lat1 - lat2)**2 + (lon1 - lon2)**2)
+    R = 6371.0  # Raio médio da Terra em quilómetros
+    lat1_rad = math.radians(lat1)
+    lon1_rad = math.radians(lon1)
+    lat2_rad = math.radians(lat2)
+    lon2_rad = math.radians(lon2)
+
+    dlon = lon2_rad - lon1_rad
+    dlat = lat2_rad - lat1_rad
+
+    # Aplicação da Fórmula de Haversine
+    a = math.sin(dlat / 2)**2 + math.cos(lat1_rad) * math.cos(lat2_rad) * math.sin(dlon / 2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+    distancia_km = R * c
+   
+    return distancia_km
--- backend/algorithms/methodwithheuristic/procura_sofrega.py
+++ backend/algorithms/methodwithheuristic/procura_sofrega.py
@@ -1,13 +1,34 @@
 from ...data.cities_graph import cidades_vizinhas
-from ...data.heuristics import distancias_retas
-from calcular_heuristica import calcular_heuristica
+from .calcular_heuristica import calcular_heuristica
 
 def procura_sofrega(cidade_origem, cidade_destino):
     caminho = [cidade_origem]
     cidade_atual = cidade_origem
+    distancia = 0
+
+    print(f"A iniciar procura sofrega de {cidade_origem} para {cidade_destino}")
 
     while cidade_atual != cidade_destino:
         proximos_nos = cidades_vizinhas.get(cidade_atual, {})
 
-        for pn in proximos_nos:
-            calcular_heuristica(pn, cidade_destino)
+        if not  proximos_nos:
+            print("Caminho não encontrado.(Sem saída)")
+            return None
+        
+        proxima_cidade = min(proximos_nos, key=lambda cidade: calcular_heuristica(cidade, cidade_destino))
+        distancia += proximos_nos[proxima_cidade]
+ 
+        h_val = calcular_heuristica(proxima_cidade, cidade_destino)
+        print(f"Interação: De {cidade_atual} escolhi {proxima_cidade} (Heurística: {h_val:.2f})")
+
+        cidade_atual = proxima_cidade
+        caminho.append(cidade_atual)
+    print(f"\nCaminho final: {caminho}")
+    print(f"Distância: {distancia}")
+    return caminho
+
+
+if __name__ == "__main__":
+    origem = "Coimbra"
+    destino = "Faro"
+    caminho_encontrado = procura_sofrega(origem, destino)
