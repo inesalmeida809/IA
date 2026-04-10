696499c3949abae27ae7ca3e1bfe469928e1b8c5
Catarina Costa <catarinac.2005@gmail.com>
2026-04-10 16:59:46 +0100

    feat: a*

--- backend/services/a_service.py
+++ backend/services/a_service.py
@@ -1,16 +1,16 @@
 from fastapi import APIRouter
-from algorithms.methodwithheuristic.procura_sofrega import procura_sofrega
+from algorithms.methodwithheuristic.a import a_star
 
 router = APIRouter()
 
-@router.get("/procura-sofrega")
-def procura_sofrega_service(chegada, destino):
-    caminho, custo = procura_sofrega(chegada, destino)
-    
+@router.get("/a-star")
+def a_star_service(chegada, destino):
+    caminho, custo = a_star(chegada, destino)
+
     if caminho is not None and custo is not None:
         return {
             "caminho": caminho,
-            "distancia": distancia,
+            "custo": custo,
         }
     else:
         return {
