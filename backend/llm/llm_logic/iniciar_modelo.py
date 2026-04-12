3ea8214ffeb00580d7e6ae85f573279a0eb1729b
Diogo Oliveira <dfso2013@gmail.com>
2026-04-12 13:54:23 +0100

    modelo quase quase

---
+++ backend/llm/llm_logic/iniciar_modelo.py
@@ -0,0 +1,27 @@
+import os
+import json
+from llama_cpp import Llama
+
+pasta_llm_logic = os.path.dirname(os.path.abspath(__file__))
+pasta_llm = os.path.dirname(pasta_llm_logic)
+pasta_modelo = os.path.join(pasta_llm, "model_IA")
+
+ficheiro_model = "Qwen2.5-3B-Instruct-Q4_K_M.gguf"
+caminho_completo = os.path.join(pasta_modelo, ficheiro_model)
+
+def iniciar_modelo():
+    if not os.path.exists(caminho_completo):
+        print("Erro: O modelo não foi encontrado.")
+        return
+
+    print("🤖 A carregar o modelo...")
+    
+    llm = Llama(
+        model_path=caminho_completo,
+        n_ctx=1024, 
+        n_gpu_layers=-1, 
+        n_threads=6,
+        verbose=False
+    )
+
+return llm
